42239b9d9689c92e71faa8178471277f1ed155ac
Nagan0 <q104084s@mail.kyutech.jp>
2020-03-28 03:23:42 +0900

    [First commit]
    ・.bashrcの追加
    ・powerline-shellのセットアップスクリプトの追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+mv ~/.vimrc ~/dotfiles/
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+mv ~/.bashrc ~/dotfiles
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
---
+++ setup_powerline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts/
+./install.sh
+cd ..
+rm -rf fonts/
+
+gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono derivative Powerline 14'
+python -m pip install --user powerline-shell
+python -m pip install --user powerline-status
+
+mkdir -p ~/.config/powerline-shell && powerline-shell --generate-config > ~/.config/powerline-shell/config.json
+
